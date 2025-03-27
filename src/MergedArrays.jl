42c784cfb5e17835da30310cafef0fd52a190cc1
Anton Oresten <antonoresten@gmail.com>
2025-03-27 17:20:44 +0100

    Initial commit

--- src/MergedArrays.jl
+++ src/MergedArrays.jl
@@ -1,5 +1,71 @@
 module MergedArrays
 
-# Write your package code here.
+using EllipsisNotation
+using ConstructionBase
+
+export Merged, merged
+
+struct MergedArray{T,A<:AbstractArray,R} <: AbstractVector{T}
+    storage::A
+    ranges::R
+end
+
+function MergedArray{T}(storage::A, ranges::R) where {T,A<:AbstractArray,R}
+    first(axes(storage, ndims(storage))) == 1 || error("storage array must have 1-based indexing")
+    return MergedArray{T,A,R}(storage, ranges)
+end
+
+Base.size(ja::MergedArray) = size(ja.ranges)
+
+Base.getindex(ja::MergedArray, i::Integer) = ja.storage[.., ja.ranges[i]]
+Base.getindex(ja::MergedArray{String}, i::Integer) = String(ja.storage[.., ja.ranges[i]])
+
+function _merge_array(arrays::AbstractVector{A}) where A<:AbstractArray
+    lengths = Iterators.map(last ∘ size, arrays)
+    cumlens = Iterators.accumulate(+, lengths, init=0)
+    ranges = [i+1:j for (i,j) in zip(Iterators.flatten((0, cumlens)), cumlens)]
+    storage = similar(A, size(first(arrays))[1:end-1]..., sum(lengths))
+    for (array, range) in zip(arrays, ranges)
+        @inbounds storage[.., range] .= array
+    end
+    return storage, ranges
+end
+
+function _merge(arrays::AbstractVector{A}) where A<:AbstractArray
+    storage, ranges = _merge_array(arrays)
+    return MergedArray{typeof(storage)}(storage, ranges)
+end
+
+function _merge(strings::AbstractVector{S}) where S<:AbstractString
+    storage, ranges = _merge_array(collect.(codeunits.(strings)))
+    return MergedArray{String}(storage, ranges)
+end
+
+_merge(xs::AbstractVector) = xs
+
+
+struct Merged{T,S<:NamedTuple,C} <: AbstractVector{T}
+    storage::S
+    len::Int
+    constructor::C
+end
+
+function Merged{T}(storage::S, len::Int) where {T,S<:NamedTuple}
+    constructor = constructorof(T)
+    return Merged{T,S,typeof(constructor)}(storage, len, constructor)
+end
+
+Base.size(m::Merged) = (m.len,)
+
+Base.getindex(m::Merged, i::Integer) = m.constructor(map(v -> v[i], m.storage)...)
+
+function merged(xs::AbstractVector{T}) where T
+    d = Dict()
+    for name in fieldnames(T)
+        d[name] = _merge([getfield(x, name) for x in xs])
+    end
+    storage = (; zip(keys(d), values(d))...)
+    return Merged{T}(storage, length(xs))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
 using MergedArrays
 using Test
 
+
+
 @testset "MergedArrays.jl" begin
-    # Write your tests here.
+
+    @testset "structs" begin
+        struct A
+            a
+            b
+            c
+        end
+
+        Base.:(==)(a::A, b::A) = a.a == b.a && a.b == b.b && a.c == b.c
+
+        arr = [A("ABC", [1,2,3], [0 0]), A("DEFG", [4,5,6,7], [1 1 1])]
+        marr = merged(arr)
+        @test marr isa Merged
+        @test marr isa AbstractVector{A}
+        @test marr[1] isa A
+        @test all(marr .== arr)
+    end
+
+    @testset "NamedTuple" begin
+        arr = [
+            (a="ABC", b=[1,2,3], c=[0 0]),
+            (a="DEFG", b=[4,5,6,7], c=[1 1 1])
+        ]
+        marr = merged(arr)
+        @test marr isa Merged
+        @test marr isa AbstractVector{eltype(arr)}
+        @test marr[1] isa eltype(marr)
+        @test all(marr .== arr)
+    end
+
 end
