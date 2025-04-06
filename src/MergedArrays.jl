cea425ea9a39d665472f7370fdd78c471ea8b33b
Anton Oresten <antonoresten@gmail.com>
2025-04-06 11:48:52 +0200

    clean up code

--- src/MergedArrays.jl
+++ src/MergedArrays.jl
@@ -3,9 +3,9 @@ module MergedArrays
 import ArraysOfArrays
 using ConstructionBase
 
-export AbstractMergedArray, AbstractMergedVector, AbstractMergedMatrix
-export MergedArray, MergedVector, MergedMatrix
-export MergedArrayOfArrays, MergedVectorOfArrays, MergedMatrixOfArrays
+export AbstractMergedArray,  AbstractMergedVector,  AbstractMergedMatrix
+export MergedArray,          MergedVector,          MergedMatrix
+export MergedArrayOfArrays,  MergedVectorOfArrays,  MergedMatrixOfArrays
 export MergedArrayOfStrings, MergedVectorOfStrings, MergedMatrixOfStrings
 
 # main API
@@ -17,42 +17,28 @@ abstract type AbstractMergedArray{T,N} <: AbstractArray{T,N} end
 const AbstractMergedVector = AbstractMergedArray{<:Any,1}
 const AbstractMergedMatrix = AbstractMergedArray{<:Any,2}
 
-# only for pretty-printing
+# pretty-printing
 parent_type(@nospecialize array::AbstractMergedArray) = array._parent_type
+Base.showarg(io::IO, array::AbstractMergedArray, _) = print(io, "merged(::$(parent_type(array)))")
 
-function Base.showarg(io::IO, array::AbstractMergedArray, toplevel)
-    print(io, "merged(::$(parent_type(array)))")
-end
-
+Base.getindex(array::AbstractMergedArray, i::Integer...) = array[LinearIndices(array)[i...]]
+Base.getindex(array::AbstractMergedArray, i::CartesianIndex) = array[LinearIndices(array)[i]]
 Base.getindex(array::AbstractMergedArray, I...) = [array[i] for i in LinearIndices(array)[I...]]
 
 
 struct MergedArray{T,N,S<:NamedTuple,C,P} <: AbstractMergedArray{T,N}
     _eltype::Type{T}
-    _storage::S
     _size::Dims{N}
+    _storage::S
     _constructor::C
     _parent_type::Type{P}
 end
 
-function MergedArray{T}(storage::S, size::Dims{N}, _parent_type::Type) where {T,N,S<:NamedTuple}
-    return MergedArray(T, storage, size, constructorof(T), _parent_type)
-end
-
-const MergedVector = MergedArray{<:Any,1}
-const MergedMatrix = MergedArray{<:Any,2}
-
 Base.size(array::MergedArray) = array._size
+Base.getindex(array::MergedArray, i::Integer) = array._constructor(map(v -> v[i], array._storage)...)
 
-Base.getindex(array::MergedArray, i::Integer...) = array._constructor(map(v -> v[i...], array._storage)...)
-
-function Base.getproperty(array::MergedArray, prop::Symbol)
-    if prop in fieldnames(MergedArray)
-        return getfield(array, prop)
-    else
-        return getproperty(array._storage, prop)
-    end
-end
+Base.getproperty(array::MergedArray, prop::Symbol) = prop in fieldnames(MergedArray) ?
+    getfield(array, prop) : getproperty(getfield(array, :_storage), prop)
 
 function MergedArray(xs::AbstractArray{T}) where T
     pairs = Pair{Symbol,Any}[]
@@ -60,31 +46,25 @@ function MergedArray(xs::AbstractArray{T}) where T
         push!(pairs, name => merged([getfield(x, name) for x in xs]))
     end
     storage = (; pairs...)
-    newT = typeof(constructorof(T)(first.(last.(pairs))...))
-    return MergedArray{newT}(storage, size(xs), typeof(xs))
+    constructor = constructorof(T)
+    x = typeof(constructor(first.(last.(pairs))...))
+    return MergedArray(typeof(x), size(xs), storage, constructor, typeof(xs))
 end
 
 
-# ArraysOfArrays.VectorOfArrays with arbitrary dimensions
-# constrained in that it's not mutable/resizeable like ArraysOfArrays.VectorOfArrays
+# essentially ArraysOfArrays.VectorOfArrays with arbitrary dimensions
 struct MergedArrayOfArrays{T,N,M,S<:ArraysOfArrays.VectorOfArrays{T,M},P} <: AbstractMergedArray{Array{T,M},N}
     storage::S
     size::Dims{N}
     _parent_type::Type{P}
 end
 
-const MergedVectorOfArrays = MergedArrayOfArrays{<:Any,1}
-const MergedMatrixOfArrays = MergedArrayOfArrays{<:Any,2}
+Base.size(array::MergedArrayOfArrays) = array.size
+Base.getindex(array::MergedArrayOfArrays, i::Integer) = collect(array.storage[i])
 
 function MergedArrayOfArrays(arrays::AbstractArray{<:AbstractArray})
     return MergedArrayOfArrays(ArraysOfArrays.VectorOfArrays(vec(arrays)), size(arrays), typeof(arrays))
 end
-
-Base.size(array::MergedArrayOfArrays) = array.size
-
-Base.getindex(array::MergedArrayOfArrays, i::Integer) = collect(array.storage[i])
-Base.getindex(array::MergedArrayOfArrays, i::Integer...) = array[LinearIndices(array)[i...]]
-Base.getindex(array::MergedArrayOfArrays, i::CartesianIndex) = array[LinearIndices(array)[i]]
 
 
 struct MergedArrayOfStrings{N,A<:MergedArrayOfArrays{<:Any,N},P} <: AbstractMergedArray{String,N}
@@ -92,17 +72,22 @@ struct MergedArrayOfStrings{N,A<:MergedArrayOfArrays{<:Any,N},P} <: AbstractMerg
     _parent_type::Type{P}
 end
 
-const MergedVectorOfStrings = MergedArrayOfStrings{1}
-const MergedMatrixOfStrings = MergedArrayOfStrings{2}
+Base.size(strings::MergedArrayOfStrings) = size(strings.array)
+Base.getindex(strings::MergedArrayOfStrings, i::Integer) = String(strings.array[i])
 
 function MergedArrayOfStrings(strings::AbstractArray{<:AbstractString})
     return MergedArrayOfStrings(MergedArrayOfArrays(codeunits.(strings)), typeof(strings))
 end
 
-Base.size(strings::MergedArrayOfStrings) = size(strings.array)
 
-Base.getindex(strings::MergedArrayOfStrings, i::Integer...) = String(strings.array[i...])
-Base.getindex(strings::MergedArrayOfStrings, i::CartesianIndex) = String(strings.array[i])
+const MergedVector = MergedArray{<:Any,1}
+const MergedMatrix = MergedArray{<:Any,2}
+
+const MergedVectorOfArrays = MergedArrayOfArrays{<:Any,1}
+const MergedMatrixOfArrays = MergedArrayOfArrays{<:Any,2}
+
+const MergedVectorOfStrings = MergedArrayOfStrings{1}
+const MergedMatrixOfStrings = MergedArrayOfStrings{2}
 
 
 """
@@ -114,7 +99,7 @@ references and reduce strain on the garbage collector.
 function merged end
 
 # Any[1, 2] -> Int[1, 2]
-# Any[1, 1.0] -> Real[1, 1.0]
+# Any[1, 2.0] -> Real[1, 2.0]
 # Vector{Any}[[1], Any[2]] -> Vector{Int}[[1], [2]]
 nested_narrow(x) = identity(x)
 nested_narrow(array::AbstractArray) = nested_narrow.(array)
